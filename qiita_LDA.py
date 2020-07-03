6e2e1bc33b7b4be0ccc29e2db3e22e4614552d5d
Arasou-0206 <49772067+Arasou-0206@users.noreply.github.com>
2020-07-03 13:27:05 +0900

    Update qiita_LDA.py

--- qiita_LDA.py
+++ qiita_LDA.py
@@ -8,11 +8,17 @@ import re
 import numpy as np
 from collections import Counter
 from sklearn import datasets
-import matplotlib.pyplot as plt
+import matplotlib
+import matplotlib.pylab as plt
 from wordcloud import WordCloud
 from scipy.special import digamma
-
+import MeCab
 from pyquery import PyQuery as pq
+from tqdm import tqdm
+import pyLDAvis
+import pyLDAvis.gensim
+
+
 p = re.compile(r"<[^>]*?>")
 with open('test2.json', 'r') as f :
     jsn = json.load(f)
@@ -22,7 +28,7 @@ with open('test2.json', 'r') as f :
 docs = [" "]
 stopwords = []
 
-for v in range(500):
+for v in range(5):
     docs.extend(p.sub("",jsn[v]['rendered_body']).split())
     #print(docs)
 
@@ -34,8 +40,8 @@ texts = [
 
 #単語出現頻度#
 count = Counter(w for doc in texts for w in doc)
-print(count.most_common()[:10])
-y = [i[1] for i in count.most_common()]
+#print(count.most_common()[:10])
+#y = [i[1] for i in count.most_common()]
 #plt.plot(y)
 #plt.savefig('out_graph.png', dpi=300, orientation='portrait', transparent=False, pad_inches=0.0)
 #plt.savefig('out_graph2.pdf', orientation='portrait', transparent=False, bbox_inches=None, frameon=None)
@@ -44,27 +50,46 @@ y = [i[1] for i in count.most_common()]
 #辞書作成#
 dictionary = gensim.corpora.Dictionary(texts)
 corpus = [dictionary.doc2bow(text) for text in texts]
-#print(corpus[1])
+#頻出を絞りたい
+#num_tokens = len(count.most_common())
+#N = int(num_tokens*0.05)
+#max_frequency = count.most_common()[N][1]
+#corpus = [[w for w in doc if max_frequency > w[1] >= 3] for doc in corpus]
 
-#トピックモデル学習
-num_topics = 20
- 
-lda = gensim.models.ldamodel.LdaModel(
-    corpus=corpus,
-    num_topics=num_topics,
-    id2word=dictionary
-)
+#lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus, id2word=dictionary, num_topics=8, random_state=0)
+#pyLDAvis.enable_notebook()
+#vis = pyLDAvis.gensim.prepare(lda_model, corpus, dictionary, sort_topics=False)
+#vis
+#pyLDAvis.save_html(vis, 'pyldavis_output.html')
 
-#表示
-plt.figure(figsize=(40,40))
-for t in range(lda.num_topics):
-    plt.subplot(5,4,t+1)
-    x = dict(lda.show_topic(t,300))
-    im = WordCloud().generate_from_frequencies(x)
-    plt.imshow(im)
-    plt.axis("off")
-    plt.title("Topic #" + str(t))
+start = 2
+limit = 30
+step = 2
 
-plt.savefig('out_graph2.png', dpi=300, orientation='portrait', transparent=False, pad_inches=0.0)
-plt.savefig('out_graph2.pdf', orientation='portrait', transparent=False, bbox_inches=None, frameon=None)
+coherence_vals = []
+perplexity_vals = []
+
+for n_topic in range(start, limit, step):
+
+    lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus, id2word=dictionary, num_topics=n_topic, random_state=0)
+    perplexity_vals.append(np.exp2(-lda_model.log_perplexity(corpus)))
+    coherence_model_lda = gensim.models.CoherenceModel(model=lda_model, texts=texts, dictionary=dictionary, coherence='c_v')
+    coherence_vals.append(coherence_model_lda.get_coherence())
+
+fig, ax1 = plt.subplots(figsize=(12,5))
+x = range(start, limit, step)
+
+c1 = 'gray'
+ax1.plot(x, coherence_vals, 'o-', color=c1)
+ax1.set_xlabel('Num Topics')
+ax1.set_ylabel('Coherence', color=c1); ax1.tick_params('y', colors=c1)
+
+c2 = 'green'
+ax2 = ax1.twinx()
+ax2.plot(x, perplexity_vals, 'o-', color=c2)
+ax2.set_ylabel('Perplexity', color=c2); ax2.tick_params('y', colors=c2)
+
+ax1.set_xticks(x)
+fig.tight_layout()
+plt.grid()
 plt.show()

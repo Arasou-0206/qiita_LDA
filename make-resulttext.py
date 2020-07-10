26d8f3dfdaa7ad49a61b9cc85b45cbcff5208c45
Arasou-0206 <49772067+Arasou-0206@users.noreply.github.com>
2020-07-10 15:22:45 +0900

    Update make-resulttext.py

--- make-resulttext.py
+++ make-resulttext.py
@@ -3,15 +3,16 @@ import json
 import csv
 from janome.tokenizer import Tokenizer
 
-t = Tokenizer()
+t = Tokenizer('neologd')
 
-with open('result.txt', 'w') as f:
+with open('result.txt', 'w', encoding='utf-8') as f:
     writer = csv.writer(f)
-    for i, row in enumerate(open('items20191218.json')):
+    for i, row in enumerate(open('qiita_data.json')):
         obj = json.loads(row)
         text = obj['rendered_body']
         soup = BeautifulSoup(text, 'html.parser')
         for e in soup.find_all('div', class_='code-frame'):
             e.clear()
-        tokens = [token.base_form for token in t.tokenize(soup.get_text())]
+        tokens = [token.base_form for token in t.tokenize(soup.get_text()) if  token.part_of_speech.startswith('名詞')]
         writer.writerow(tokens)
+        print(i)
